2052a515b9ec5ff6c2a32c6c0187b0e26962493c
van Rooyen, Devin <d.van-rooyen@liverpool.ac.uk>
2021-12-12 21:19:28 +0000

    Update server.py

--- server.py
+++ server.py
@@ -12,6 +12,7 @@ Steps:
 9. Print file contents
 """
 
+import re
 import socket
 import pickle
 import json
@@ -21,7 +22,6 @@ from dict2xml import dict2xml
 from cryptography.fernet import Fernet
 import xmltodict
 from os.path import exists as file_exists
-
 
 # Key for fernet encryption
 key = "R29kemlsbGFJc0p1c3RBSHVnZVRvYWRDYWxsZWRUaW0="
@@ -48,7 +48,6 @@ def start_server(PORT):
             sys.exit(1)
         print(f"Server Open on {HOST}:{PORT}")
         s.listen()
-        s.settimeout(30)
         conn, addr = s.accept()
         with conn:
             print(f"{addr[0]} Connected")
@@ -60,47 +59,28 @@ def start_server(PORT):
                 inc_data = repr(data)
 
 
-""" Expecting Serialization data """
-start_server(1337)
-
-full_data = inc_data[2:-1].split('~')
-
-
-if (full_data[0] == 'y'):
-    message = decrypt(full_data[1].encode('utf-8'))
-else:
-    message = full_data[1].replace('\\\\', '\\')
-    message = message.encode('utf-8')
-    message = message.decode('unicode-escape').encode('latin1')
+def stop_server():
+    print("Server Closed")
+    sys.exit(1)
 
 
 def xml_deserialize(message):
     msg_parsed = str(message)[2:-1]
     msg_deserial = xmltodict.parse(msg_parsed)
     s_dict = msg_deserial["root"]
-    s_dict.pop('#text')
-    msg_dict = dict(s_dict)
+    try:
+        s_dict.pop('#text')
+        msg_dict = dict(s_dict)
 
-    return(msg_dict)
-
-
-# De-serialize data
-if (full_data[0] == "pickle"):
-    dict = pickle.loads(message)
-if (full_data[0] == "json"):
-    dict = json.loads(message)
-if (full_data[0] == "xml"):
-    dict = xml_deserialize(message)
+        return(msg_dict)
+    except KeyError:
+        return(s_dict)
 
 
 def file_creator(content):
     # If file exists don't write, increment number
-    found = False
-    file_num = 1
-    filename = ""
-    filename = "file" + str(file_num) + ".txt"
-    found = file_exists(filename)
 
+    file_num = 1
     while(True):
         filename = "file" + str(file_num) + ".txt"
         if(file_exists(filename) is False):
@@ -108,21 +88,47 @@ def file_creator(content):
             file.write(content)
             file.close()
             print(f"{filename} created!")
-            break
+            return True
 
         file_num += 1
 
 
-# Output to screen
-if (full_data[2] == "1"):
-    print(f"You provided the server with:\n{dict}")
-if (full_data[2] == "2"):
-    file_creator(str(dict))
+def main():
+
+    # Data from socket
+    inc_data = ""
+    start_server(5000)
+
+    """ Expecting Serialization data """
+
+    full_data = inc_data[2:-1].split('~')
+
+    if (full_data[0] == 'y'):
+        message = decrypt(full_data[1].encode('utf-8'))
+    else:
+        message = full_data[1].replace('\\\\', '\\')
+        message = message.encode('utf-8')
+        message = message.decode('unicode-escape').encode('latin1')
+
+    # De-serialize data
+    if (full_data[0] == "pickle"):
+        dict_ = pickle.loads(message)
+    if (full_data[0] == "json"):
+        dict_ = json.loads(message)
+    if (full_data[0] == "xml"):
+        dict_ = xml_deserialize(message)
+    # Output to screen
+    if (full_data[2] == "1"):
+        print(f"You provided the server with:\n{dict_}")
+    if (full_data[2] == "2"):
+        file_creator(str(dict_))
+
+    """ Expecting File """
+
+    full_data = inc_data[2:-1]
+    decrypt_text = decrypt(full_data.encode('utf-8')).decode()
+    print(decrypt_text)
 
 
-""" Expecting File """
-start_server(13373)
-
-full_data = inc_data[2:-1]
-decrypt_text = decrypt(full_data.encode('utf-8')).decode()
-print(decrypt_text)
+if __name__ == "__main__":
+    main()
