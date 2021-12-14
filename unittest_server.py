293b5dd534125d91d51fff3472ef2dba6d74eaf4
van Rooyen, Devin <d.van-rooyen@liverpool.ac.uk>
2021-12-14 11:15:05 +0000

    Update unittest_server.py

--- unittest_server.py
+++ unittest_server.py
@@ -1,3 +1,12 @@
+"""Unittest client side
+
+Class tests: 
+1. Server test
+2. Decryption test
+3. File creation 
+4. Xml de-serialize test
+"""
+
 import unittest
 from server import decrypt, xml_deserialize, file_creator
 
@@ -5,17 +14,22 @@ from client import encrypt
 
 
 class TestServer(unittest.TestCase):
+
+# compare test value with true
     def test_server(self):
         self.assertTrue(True)
 
+# test quality assesrtion to the original string
     def test_decrypt_text(self):
         encrypted_text = encrypt(b"Hello World")
         decrypted_text = decrypt(encrypted_text)
         self.assertEqual(decrypted_text, b"Hello World")
 
+# file creation assesrtion 
     def test_file_creator(self):
         self.assertTrue(file_creator("test content"))
 
+# test xml de-serialize function
     def test_xml_deserialize(self):
         xml_string = b'<root><a>1</a><b>2</b></root>'
         xml_dict = xml_deserialize(xml_string)
