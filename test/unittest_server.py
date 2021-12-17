00e069c764f0970c3bcb603905a793612b3de852
van Rooyen, Devin <d.van-rooyen@liverpool.ac.uk>
2021-12-17 16:35:38 +0000

    Update test/unittest_server.py

--- test/unittest_server.py
+++ test/unittest_server.py
@@ -16,33 +16,29 @@ sys.path.insert(0, parentdir)
 from src.server import decrypt, xml_deserialize, file_creator
 from src.client import encrypt
 
-
-
 class TestServer(unittest.TestCase):
     """ Server Side Unit Tests"""
 
-    def test_decrypt_text(self):
-        """Check if string has interference after encryption and decryption"""
+    def test_encrypt_text(self):
+        """Check if string has interference after encryption"""
         encrypted_text = encrypt(b"Hello World")
-        encrypted_text != decrypted_text()
+        self.assertNotEqual(encrypted_text, b"Hello World")
+    
+    def test_decrypt_text(self):
+        """Check if string has interference after decryption"""
+        encrypted_text = encrypt(b"Hello World")
         decrypted_text = decrypt(encrypted_text)
         self.assertEqual(decrypted_text, b"Hello World")
-    
-    print("[1] test_decrypt_text passed")
-    
+
     def test_file_creator(self):
         """Test File Creation"""
         self.assertTrue(file_creator("test content", ""))
-
-    print("[2] test_file_creator passed")
 
     def test_xml_deserialize(self):
         """Test expected output from xml_deserialize function"""
         xml_string = b'<root><a>1</a><b>2</b></root>'
         xml_dict = xml_deserialize(xml_string)
         self.assertEqual(xml_dict, {'a': '1', 'b': '2'})
-
-    print("[3] test_xml_deserialize passed")
 
 
 if __name__ == '__main__':
